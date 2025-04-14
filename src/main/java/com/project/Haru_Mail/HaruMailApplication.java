043fb7481a457f916877ca958f65f7a477485af2
AYEOOON <k93853471@gmail.com>
2025-04-14 22:51:00 +0900

    init commit

---
+++ src/main/java/com/project/Haru_Mail/HaruMailApplication.java
@@ -0,0 +1,13 @@
+package com.project.Haru_Mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruMailApplication.class, args);
+	}
+
+}
