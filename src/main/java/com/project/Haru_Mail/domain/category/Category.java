60c304b35b6b6f426c3dda5800805d52d04216cc
caeyon <shallyshin@naver.com>
2025-04-16 20:51:07 +0900

    feat: 주요 엔티티 설계

--- src/main/java/com/project/Haru_Mail/domain/category/Category.java
+++ src/main/java/com/project/Haru_Mail/domain/category/Category.java
@@ -1,4 +1,19 @@
 package com.project.Haru_Mail.domain.category;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // 카테고리 ID
+
+    private String name; // 카테고리명
 }
--- src/main/java/com/project/Haru_Mail/domain/diary/Diary.java
+++ src/main/java/com/project/Haru_Mail/domain/diary/Diary.java
@@ -1,4 +1,34 @@
 package com.project.Haru_Mail.domain.diary;
 
+import com.project.Haru_Mail.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Diary")
 public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // 다이어리 ID
+
+    private String title; // 제목
+
+    @Lob
+    private String content; // 내용
+
+    private LocalDate date; // 작성 날짜
+
+    private LocalTime time; // 작성 시간
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; // 사용자 ID
 }
