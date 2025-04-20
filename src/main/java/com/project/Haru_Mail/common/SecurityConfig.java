538e12c8ae42be8157673a846d7be48d88b60593
AYEOOON <k93853471@gmail.com>
2025-04-20 15:35:10 +0900

    feat: 보안을 위한 `SecurityConfig` 클래스 추가
    - OAuth2 로그인 설정 및 사용자 인증을 위한 `OAuth2SuccessHandler` 설정

---
+++ src/main/java/com/project/Haru_Mail/common/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.project.Haru_Mail.common;
+
+import com.project.Haru_Mail.common.jwt.JwtTokenizer;
+import com.project.Haru_Mail.domain.auth.CustomOAuth2UserService;
+import com.project.Haru_Mail.domain.auth.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomOAuth2UserService oauth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Stateless session 설정
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/login/oauth2/**").permitAll()  // OAuth2 로그인 경로에 대해 접근 허용
+                                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                                .anyRequest().authenticated()  // 그 외 요청은 인증 필요
+                )
+                .oauth2Login(oauth ->
+                        oauth
+                                .userInfoEndpoint(u -> u.userService(oauth2UserService))  // OAuth2 사용자 정보 처리 서비스 설정
+                                .successHandler(oAuth2SuccessHandler)  // OAuth2 로그인 후 성공 핸들러 설정
+                );
+        return http.build();
+    }
+}
