7394ba10f1ee3e6cd894d3d1e3ff20a93a544498
caeyon <shallyshin@naver.com>
2025-04-24 20:55:27 +0900

    delete: 파일 삭제
    - 실수로 한꺼번에 푸시해서 파일 삭제 후 다시 커밋 예정....

--- src/main/java/com/project/Haru_Mail/domain/diary/DiaryTagRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.Haru_Mail.domain.diary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryTagRepository extends JpaRepository<DiaryTag, DiaryTagId> {
-}
