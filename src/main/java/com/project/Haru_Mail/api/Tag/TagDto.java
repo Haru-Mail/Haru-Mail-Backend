7ba59c986ce5ac3d8460cebfd2b60fce34b84626
caeyon <shallyshin@naver.com>
2025-04-25 21:22:56 +0900

    feat: TagDto 클래스 추가
    - 태그 생성 및 조회를 위한 `TagDto` 클래스 추가

---
+++ src/main/java/com/project/Haru_Mail/api/Tag/TagDto.java
@@ -0,0 +1,24 @@
+package com.project.Haru_Mail.api.Tag;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+public class TagDto {
+    @Data
+    @AllArgsConstructor
+    public static class TagRequestDto { //태그 생성
+        private String name;
+        private Integer categoryId;
+        private Long userId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class ShowTagDto { //태그 조회
+        private Integer categoryId;
+        private Long userId;
+    }
+}
