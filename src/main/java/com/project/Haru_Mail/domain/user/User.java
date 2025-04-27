f79872c2814020e2ce9741b07de07132be210d20
caeyon <shallyshin@naver.com>
2025-04-27 22:09:43 +0900

    refactor: userId 필드에 컬럼 매핑(@Column) 설정
    - 외래키 설정 오류를 방지하기 위해 userId 필드에 @Column(name = "user_id")를 추가

--- src/main/java/com/project/Haru_Mail/domain/user/User.java
+++ src/main/java/com/project/Haru_Mail/domain/user/User.java
@@ -16,6 +16,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @Column(nullable = false, unique = true)
