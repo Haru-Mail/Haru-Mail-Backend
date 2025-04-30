221846cdc0c0d465aedc5fad485a78cb7ee09b97
AYEOOON <k93853471@gmail.com>
2025-04-30 23:05:00 +0900

    refactor: 사용되지 않는 어노테이션 제거

--- src/main/java/com/project/Haru_Mail/domain/question/Question.java
+++ src/main/java/com/project/Haru_Mail/domain/question/Question.java
@@ -5,14 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
