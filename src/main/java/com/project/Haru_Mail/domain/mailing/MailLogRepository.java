ea241d12526753894a55f2ea9078e51a7269a5e2
AYEOOON <k93853471@gmail.com>
2025-05-01 15:43:20 +0900

    feat: MailLogRepository 추가

---
+++ src/main/java/com/project/Haru_Mail/domain/mailing/MailLogRepository.java
@@ -0,0 +1,6 @@
+package com.project.Haru_Mail.domain.mailing;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MailLogRepository extends CrudRepository<MailLog, Long> {
+}
