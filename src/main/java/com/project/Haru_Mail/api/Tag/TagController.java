704d7ee7c77770df7cf3a602247da384936abfbe
caeyon <shallyshin@naver.com>
2025-05-14 18:27:56 +0900

    feat: 태그 저장 시 사용자 정보 연동 로직 추가
    - TagController의 createTag 메서드에서 HttpServletRequest를 통해 현재 로그인한 사용자 정보를 가져오도록 수정
    - 생성되는 태그에 사용자 정보를 백엔드에서 가져와 연결한 후에 사용자 기반 태그 생성 가능하도록 개선

--- src/main/java/com/project/Haru_Mail/api/Tag/TagController.java
+++ src/main/java/com/project/Haru_Mail/api/Tag/TagController.java
@@ -1,11 +1,18 @@
 package com.project.Haru_Mail.api.Tag;
 
+import com.project.Haru_Mail.domain.diary.Diary;
+import com.project.Haru_Mail.domain.diary.DiaryTagService;
 import com.project.Haru_Mail.domain.diary.Tag;
 import com.project.Haru_Mail.domain.diary.TagService;
+import com.project.Haru_Mail.domain.user.User;
+import com.project.Haru_Mail.domain.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tag")
@@ -13,11 +20,15 @@ import org.springframework.web.bind.annotation.*;
 public class TagController {
 
     private final TagService tagService;
+    private final UserService userService;
+    private final DiaryTagService diaryTagService;
 
     // 태그 생성 API
     @PostMapping("/create")
-    public ResponseEntity<Tag> createTag(@RequestBody TagDto.TagRequestDto requestDto) {
-        Tag tag = tagService.createTag(requestDto);  // 서비스에서 태그 생성
+    public ResponseEntity<Tag> createTag(@RequestBody TagDto.TagRequestDto requestDto,
+                                         HttpServletRequest httpServletRequest) {
+        User currentUser = userService.getCurrentUser(httpServletRequest);
+        Tag tag = tagService.createTag(requestDto, currentUser);  // 서비스에서 태그 생성
         return new ResponseEntity<>(tag, HttpStatus.CREATED); // 생성된 태그 반환 or 아이디만 반환
     }
 
