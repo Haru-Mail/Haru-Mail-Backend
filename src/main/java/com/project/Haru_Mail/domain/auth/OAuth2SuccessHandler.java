adb353dd3b398356ff8226a29ca8140d59738423
AYEOOON <k93853471@gmail.com>
2025-06-11 16:21:19 +0900

    fix: 로그인 후 리디렉션 링크를 /list로 변경

--- src/main/java/com/project/Haru_Mail/domain/auth/OAuth2SuccessHandler.java
+++ src/main/java/com/project/Haru_Mail/domain/auth/OAuth2SuccessHandler.java
@@ -40,7 +40,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("Redis 저장 완료 : " + email);
 
         // 로그인 성공 후 리다이렉트 (사용자 정보는 URL에 포함하지 않음)
-        getRedirectStrategy().sendRedirect(request, response, "http://localhost:5173/oauth/callback");
+        getRedirectStrategy().sendRedirect(request, response, "http://localhost:5173/list");
     }
 
     private void updateTokensInCookies(HttpServletResponse response, String email) {
